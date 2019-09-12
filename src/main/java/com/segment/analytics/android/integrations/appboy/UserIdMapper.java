53b500e5e05ad8c92f5ec7ee5bdc9f981a81b355
Ivan Garza <ivan.garza@scmspain.com>
2019-09-12 09:18:09 +0200

    Added some documentation

--- src/main/java/com/segment/analytics/android/integrations/appboy/UserIdMapper.java
+++ src/main/java/com/segment/analytics/android/integrations/appboy/UserIdMapper.java
@@ -5,13 +5,10 @@ import com.appboy.Appboy;
 public interface UserIdMapper {
 
   /**
+   * Defines a transformation for all userIds before being reported to the Braze SDK
    *
-   * changeUser
-   *
-   * Returns the userId as should be reported to Braze to the {@link Appboy#changeUser(String)} method.
-   *
-   * @param segmentUserId
-   * @return
+   * @param segmentUserId user id reported to segment in identify calls
+   * @return userId as should be reported to Braze in the {@link Appboy#changeUser(String)} method.
    */
   String transformUserId(String segmentUserId);
 }
