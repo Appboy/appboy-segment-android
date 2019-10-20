9ec3e4992d35f3a01d5f2ebcb9bd4dcf325bfb87
Egor Neliuba <eneliuba@gmail.com>
2019-10-20 13:07:58 +0300

    Migrate to AndroidX

--- src/androidTest/java/com/segment/analytics/android/integrations/appboy/AppboyAndroidTest.java
+++ src/androidTest/java/com/segment/analytics/android/integrations/appboy/AppboyAndroidTest.java
@@ -1,7 +1,6 @@
 package com.segment.analytics.android.integrations.appboy;
 
-import android.support.test.runner.AndroidJUnit4;
-
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import com.appboy.Appboy;
 import com.appboy.configuration.AppboyConfig;
 import com.segment.analytics.Analytics;
@@ -9,12 +8,11 @@ import com.segment.analytics.Traits;
 import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.test.IdentifyPayloadBuilder;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.InstrumentationRegistry.getContext;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static com.segment.analytics.Utils.createTraits;
 import static org.junit.Assert.assertEquals;
 
@@ -24,7 +22,7 @@ public class AppboyAndroidTest {
   @BeforeClass
   public static void beforeClass() {
     AppboyConfig appboyConfig = new AppboyConfig.Builder().setApiKey("testkey").build();
-    Appboy.configure(getContext(), appboyConfig);
+    Appboy.configure(getApplicationContext(), appboyConfig);
   }
 
   @Test
@@ -33,9 +31,9 @@ public class AppboyAndroidTest {
     Traits traits = createTraits(testUserId);
     IdentifyPayload identifyPayload = new IdentifyPayloadBuilder().traits(traits).build();
     Logger logger = Logger.with(Analytics.LogLevel.DEBUG);
-    AppboyIntegration integration = new AppboyIntegration(Appboy.getInstance(getContext()), "foo", logger, true);
+    AppboyIntegration integration = new AppboyIntegration(Appboy.getInstance(getApplicationContext()), "foo", logger, true);
     integration.identify(identifyPayload);
 
-    assertEquals(testUserId, Appboy.getInstance(getContext()).getCurrentUser().getUserId());
+    assertEquals(testUserId, Appboy.getInstance(getApplicationContext()).getCurrentUser().getUserId());
   }
 }
